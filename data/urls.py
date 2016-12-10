81f78beef926b33512d61c3ce3332092ce11427f
Ubuntu <ubuntu@ip-172-31-44-162.us-west-2.compute.internal>
2016-12-10 04:09:18 +0000

    Comparing two teams now "works"

--- data/urls.py
+++ data/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name="index"),
+	url(r'^$', views.select, name="select"),
+	url(r'^(?P<team1>\w+)/(?P<team2>\w+)/(?P<statType>\w+)/$', views.compare, name="compare"),
 ]
--- data/views.py
+++ data/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render
 from django.template import loader
 from .models import Conference, Team, Player
+from . import StatInterface
 
-def index(request):
+gameStatsLabels =  ['NAME', 'GP', 'MIN', 'PPG', 'RPG', 'APG', 'SPG', 'BPG', 'TPG', 'FG%', 'FT%', '3P%']
+seasonStatsLabels = ['NAME', 'MIN', 'FGM', 'FGA', 'FTM', 'FTA', '3PM', '3PA', 'PTS', 'OFFR', 'DEFR', 'REB', 'AST', 'TO', 'STL', 'BLK']
+
+
+def select(request):
 	conf_list = Conference.objects.all()
-	conf1 = request.GET.get('conference1')
-	conf2 = request.GET.get('conference2')
+	conf1 = request.GET.get('c1')
+	conf2 = request.GET.get('c2')
 	team1_list = None
 	team2_list = None
-	team1_name = request.GET.get('team1')
-	team2_name = request.GET.get('team2')
-	team1_player_set = None
-	team2_player_set = None
+	team1_name = request.GET.get('t1')
+	team2_name = request.GET.get('t2')
+	stat_list = request.GET.get('statlist')
 	
 	if(conf1 != None and conf2 != None):
 		for conf in conf_list:
@@ -21,14 +25,6 @@ def index(request):
 				team2conf = conf
 		team1_list = team1conf.team_set.all()
 		team2_list = team2conf.team_set.all()
-
-	if(team1_name != None and team2_name != None):
-		for team in team1_list:
-			if(team.name.replace(' ','') == team1_name):
-				team1_player_set = team.player_set.all()
-		for team in team2_list:
-			if(team.name.replace(' ','') == team2_name):
-				team2_player_set = team.player_set.all()
 	
 	context = {
 		"conf_list" : conf_list,
@@ -38,8 +34,86 @@ def index(request):
 		"team2_list" : team2_list,
 		"team1" : team1_name,
 		"team2" : team2_name,
-		"team1_player_set" : team1_player_set,
-		"team2_player_set" : team2_player_set,
+		"stat_list" : stat_list,
 	}
 	
-	return render(request, 'data/index.html', context)
+	return render(request, 'data/select.html', context)
+
+def compare(request, team1='', team2='', statType='game'):
+	weights = None
+	favored = None
+	if statType == 'game':
+		labels = gameStatsLabels
+	elif statType == 'season':
+		labels = seasonStatsLabels
+	else:
+		statType = 'game'
+		invalidParam = True
+		labels = game
+	
+	team1_obj = StatInterface.getTeam(team1, statType)
+	team1_stat_totals = calcTotals(team1_obj, labels)
+	team1_stat_weighted = {}
+	team2_obj = StatInterface.getTeam(team2, statType)
+	team2_stat_totals = calcTotals(team2_obj, labels)
+	team2_stat_weighted = {}
+
+	if team1_obj != None and team2_obj != None:
+		#Get the user's weights
+		weights = {}
+		for label in labels:
+			weights[label] = request.GET.get(label.replace('%','_'))
+			if weights[label] == None:
+				weights[label] = "0.0"
+
+		#Applying the weights
+		team1_total = 0
+		team2_total = 0
+		for label in labels:
+			if label == 'NAME':
+				continue
+			team1_stat_weighted[label] = float(team1_stat_totals[label]) * float(weights[label])
+			team1_total += team1_stat_weighted[label]
+			team2_stat_weighted[label] = float(team2_stat_totals[label]) * float(weights[label])
+			team2_total += team2_stat_weighted[label]
+
+		if team1_total > team2_total:
+			favored = team1
+		elif team1_total < team2_total:
+			favored = team2a
+	#endif
+
+	context = {
+		"team1" : team1,
+		"team2" : team2,
+		"statType" : statType,
+		"labels" : labels,
+		"weights" : weights,
+		"team1_obj" : team1_obj,
+		"team1_totals" : team1_stat_totals,
+		"team1_stat_weighted" : team1_stat_weighted,
+		"team2_obj" : team2_obj,
+		"team2_totals" : team2_stat_totals,
+		"team2_stat_weighted" : team2_stat_weighted,
+		"favored" : favored,
+	}
+	return render(request, 'data/compare.html', context)
+
+def calcTotals(team_obj, labels):
+	if team_obj == None:
+		return None
+	team_stat_totals = {}
+	
+	#initialize all values to zero
+	for stat in labels:
+		if stat == 'NAME':
+			continue
+		team_stat_totals[stat] = 0
+
+	for player in team_obj:
+		for stat in labels:
+			if stat == 'NAME':
+				continue
+			team_stat_totals[stat] += player[stat]
+
+	return team_stat_totals
