15f1e5485ef21f1b9b5b4222fe9b34b1f2dbad9c
Ubuntu <ubuntu@ip-172-31-44-162.us-west-2.compute.internal>
2016-12-10 21:22:34 +0000

    you can now query the database for information about Conferences, Teams, and players

--- API/urls.py
+++ API/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    url(r'conferences/$', views.list_conferences, name="list_conferences"),
+    url(r'teams/$', views.list_teams, name="list_teams_none"),
+    url(r'teams/(?P<conference>\w+)/$', views.list_teams, name="list_teams_conf"),
     url(r'(?P<team>\w+)/(?P<table>\w+)/$', views.get_team, name="get_team"),
 ]
--- API/views.py
+++ API/views.py
@@ -1,11 +1,52 @@
 import io
 import json
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 from data import StatInterface
 
 gameStatsLabels =  ['NAME', 'GP', 'MIN', 'PPG', 'RPG', 'APG', 'SPG', 'BPG', 'TPG', 'FG%', 'FT%', '3P%']
 seasonStatsLabels = ['NAME', 'MIN', 'FGM', 'FGA', 'FTM', 'FTA', '3PM', '3PA', 'PTS', 'OFFR', 'DEFR', 'REB', 'AST', 'TO', 'STL', 'BLK']
+
+def list_conferences(request):
+    conf_list = StatInterface.getConfList()
+
+    conf_dict = {}
+    data = io.StringIO()
+    data.write('[')
+    for conf in conf_list:
+        conf_dict[conf] = scrub(conf)
+    data.write(str(json.dumps(conf_dict, sort_keys=True)))
+    data.write(']')
+
+    return_string = data.getvalue()
+    return_string = return_string.replace(',', ',\n')
+
+    return HttpResponse(return_string, content_type='text/plain')
+
+def list_teams(request, conference=None):
+    team_dict = {}
+    if conference != None:
+        team_list = StatInterface.getTeamList(unscrub(conference))
+        if team_list == None:
+            return HttpResponseNotFound('<h1>Conference does not exist.</h1>')
+        for team in team_list:
+            team_dict[team] = scrub(team)
+    else:
+        conf_list = StatInterface.getConfList()
+        for conf in conf_list:
+            team_list = StatInterface.getTeamList(conf)
+            for team in team_list:
+                team_dict[team] = scrub(team)
+
+    data = io.StringIO()
+    data.write('[')
+    data.write(str(json.dumps(team_dict, sort_keys=True)))
+    data.write(']')
+    return_string = data.getvalue()
+    return_string = return_string.replace(',',',\n')
+
+    return HttpResponse(return_string, content_type="text/plain")
+        
 
 def get_team(request, team, table):
     team = unscrub(team)
@@ -14,9 +55,11 @@ def get_team(request, team, table):
     elif table  == 'season':
         lables = seasonStatsLabels
     else:
-        return None
+        return HttpResponseNotFound('<h1>Invalid table specified.</h1>')
 
     team_obj = StatInterface.getTeam(team, table)
+    if team_obj == None:
+        return HttpResponseNotFound('<h1>Team does not exist in database</h1>')
     count = 0
     data = io.StringIO()
     data.write('[')
@@ -24,13 +67,21 @@ def get_team(request, team, table):
         data.write(str(json.dumps(player)))
         if count < (len(team_obj) - 1):
             count += 1
-            data.write(',')
+            data.write(',\n')
     data.write(']')
 
     response = HttpResponse(data.getvalue(), content_type='text/plain')
     return response
     
-        
+def scrub(value):
+    value = value.replace(';','9')
+    value = value.replace('&','8')
+    value = value.replace('(','7')
+    value = value.replace(')','6')
+    value = value.replace(' ','_')
+    value = value.replace('\'','5')
+    return value
+
 def unscrub(value):
     value = value.replace('9', ';')
     value = value.replace('8', '&')
